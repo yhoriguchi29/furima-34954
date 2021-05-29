113630f1cf4374db3183bee9f91306a5ad4deb96
HOYU <yuhi.horiguchi@gmail.com>
2021-05-29 14:54:02 +0900

    商品情報編集２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: [:edit]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @item = Item.all.order('created_at DESC')
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     @item.update(item_params)
     if @item.save
       redirect_to item_path
@@ -44,7 +42,11 @@ class ItemsController < ApplicationController
                                  :ship_date_id, :sales_price).merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
   def move_to_index
-    redirect_to action: :index unless user_signed_in? && current_user.id == Item.find(params[:id]).user_id
+    redirect_to action: :index unless current_user.id == Item.find(params[:id]).user_id
   end
 end
