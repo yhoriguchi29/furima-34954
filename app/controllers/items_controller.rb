15d960ccae726bb8cf32977f56929fe96801b59f
HOYU <yuhi.horiguchi@gmail.com>
2021-05-29 15:01:41 +0900

    商品情報編集３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -47,6 +47,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index unless current_user.id == Item.find(params[:id]).user_id
+    redirect_to action: :index unless current_user.id == @item.user_id
   end
 end
